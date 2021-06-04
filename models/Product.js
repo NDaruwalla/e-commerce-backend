c8e9d7b46334561c242ac4f7a6f33b3a9ae9432f
Nicole Daruwalla <nicole.daruwalla@gmail.com>
2021-06-04 11:25:23 -0400

    defined columns for product.js

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,61 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    // `Product`
+    // * `id`
+    //   * Integer.    
+    //   * Doesn't allow null values.   
+    //   * Set as primary key.   
+    //   * Uses auto increment. 
+    // * `product_name`   
+    //   * String.   
+    //   * Doesn't allow null values. 
+    // * `price`   
+    //   * Decimal.   
+    //   * Doesn't allow null values.   
+    //   * Validates that the value is a decimal.  
+    // * `stock`    
+    //   * Integer.  
+    //   * Doesn't allow null values.   
+    //   * Set a default value of `10`.   
+    //   * Validates that the value is numeric. 
+    // * `category_id`    
+    //   * Integer.    
+    //   * References the `Category` model's `id`.
+  
+
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
