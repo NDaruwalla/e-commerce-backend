1ddac81632785a006b14336f9a9af2a2d57df887
Nicole Daruwalla <nicole.daruwalla@gmail.com>
2021-06-04 16:46:25 -0400

    created routes for category-routes.js for delete a category by its uid=197609(Nicole Daruwalla) gid=197121 groups=197121 value

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -71,7 +71,7 @@ router.put('/:id', (req, res) => {
     })
     .then(categoryData => {
       if (!categoryData) {
-        res.status(404).json({ message: 'No Category found with that ID.' });
+        res.status(404).json({ message: 'There is no category with that ID.' });
         return;
       }
       res.json(categoryData);
@@ -84,6 +84,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(categoryData => {
+      if (!categoryData) {
+        res.status(404).json({ message: 'There is no category with that ID.' });
+        return;
+      }
+      res.json(categoryData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
