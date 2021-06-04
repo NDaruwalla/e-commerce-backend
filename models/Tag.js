8d019fb248f1c49526db3302e977fb86f55405c7
Nicole Daruwalla <nicole.daruwalla@gmail.com>
2021-06-04 12:42:16 -0400

    defined columns for Tag.js

--- models/Tag.js
+++ models/Tag.js
@@ -7,6 +7,24 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+  //   * `Tag`
+  // * `id`
+  //   * Integer.
+   //   * Doesn't allow null values.
+   //   * Set as primary key.
+  //   * Uses auto increment.
+  // * `tag_name`
+  //   * String.
+
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.STRING
+    }
   },
   {
     sequelize,
