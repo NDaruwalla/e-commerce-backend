158d0a421e9b5d6b116e75d59c2e85021618e6ff
Nicole Daruwalla <nicole.daruwalla@gmail.com>
2021-06-04 19:27:37 -0400

    finished the rest of the tag routes

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const apiRoutes = require('./api');
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
-  res.send("<h1>Woooo! This is the wrong route, Buddy!</h1>")
+  res.send("<h1>Sorry, Buddy! This is the wrong route.</h1>")
 });
 
 module.exports = router;
